70afd697911ecb56d7d933c6da9ccb94cbb6880c
Théo Lopès-Quintas <63822750+theo-lq@users.noreply.github.com>
2020-04-17 11:59:15 +0200

    Update Partial_Dependance_Plot_Points.py

--- Partial_Dependance_Plot_Points.py
+++ Partial_Dependance_Plot_Points.py
@@ -7,26 +7,26 @@ import pandas as pd
 
 def partial_dependance_plot_points(model, X, column, class_of_interest=1):
     """Return the abscissa and ordinate of the partial dependance plot of the variable column in the X dataframe or array, for a classifier model.
-    
+
     Usage::
         >>> #we assume we have fitted a model (e.g. a RandomForestClassifier) for a classification problem
         >>> x, y = partial_dependance_plot_points(model, X_test, "Age")
         >>> import matplotlib.pyplot as plt
         >>> plt.plot(x, y)
-    :param: model: a classifier model e.g. from sklearn`
-            X : A pandas DataFrame or numpy array
-            column : A string
-            class_of_interest : An integer
+    :param model: a classifier model e.g. from sklearn.
+    :param X: A pandas DataFrame or numpy array.
+    :param column: A string.
+    :param class_of_interest: An integer.
     :rtype: two list of floats
     """
-    
-    
+
+
     def change_value(value):
         """Replace in X the values of the column column by value"""
         X_new = X.copy()
         X_new[column] = value * np.ones(X_new.shape[0])
         return X_new
-    
+
     abscissa = np.linspace(start=min(X[column]), stop=max(X[column]), num=100)
     ordinate = []
 
@@ -34,5 +34,5 @@ def partial_dependance_plot_points(model, X, column, class_of_interest=1):
         X_new = change_value(value)
         y_pred_proba = model.predict_proba(X_new)
         ordinate.append(np.mean(y_pred_proba[:, class_of_interest]))
-    
+
     return abscissa, ordinate
